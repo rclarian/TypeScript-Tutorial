beb0177851b7b9132ae656d9b24aad178e90843b
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-07 19:14:20 +0800

    #6 - Dynamic (any) Types
    #6 - Dynamic (any) Types

--- sandbox.ts
+++ sandbox.ts
@@ -1,39 +1,24 @@
-//explicit types
-let character: string = 'mario';
-let age: number;
-let isLoggedIn: boolean;
 
-// age = 'luigi';
-age = 30;
+let age: any = 25;
 
-// isLoggedIn = 25;
-isLoggedIn = true;
+age = true;
+console.log(age);
+age = 'hello';
+console.log(age);
+age = { name: 'luigi' };
+console.log(age);
 
-// arrays
-let ninjas: string[] = [];
+let mixed: any[] = [];
 
-ninjas.push('ryu');
-ninjas.push('chun-li');
-console.log(ninjas);
-
-// union types
-let mixed: (string|number|boolean)[] = [];
-mixed.push('hello');
+mixed.push(5);
+mixed.push('mario');
 mixed.push(false);
-mixed.push(20);
 console.log(mixed);
 
-let uid: string|number;
-uid = '123';
-uid = 123;
+let ninja: { name: any, age: any };
 
-// objects
-let ninjaOne: object;
-ninjaOne = { name: 'yoshi', age: 30 };
+ninja = { name: 'yoshi', age: 25 };
+console.log(ninja);
 
-let ninjaTwo: {
-  name: string,
-  age: number,
-  beltColour: string
-};
-ninjaTwo = { name: 'ken', age: 20, beltColour: 'black' };
+ninja = { name: 25, age: 'yoshi' };
+console.log(ninja);
