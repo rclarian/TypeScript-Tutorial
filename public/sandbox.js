59eb809a874e76318a9c8c4de116e29c357c434b
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-10 21:01:14 +0800

    #8 - Function Basics
    #8 - Function Basics

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,2 +1,18 @@
 "use strict";
-console.log('test');
+let greet = () => {
+    console.log('hello, world');
+};
+// greet = 'hello';
+greet = () => {
+    console.log('hello, again');
+};
+const add = (a, b, c /*?*/ = 10) => {
+    console.log(a + b);
+    console.log(c);
+};
+add(5, 10, 'ninja');
+const minus = (a, b) => {
+    return a + b;
+};
+let result = minus(10, 7);
+console.log(result);
