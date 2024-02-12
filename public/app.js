4523e367be00f59b033c2fa9b47cd007b39d2218
Ryan Clarian <55990450+rclarian@users.noreply.github.com>
2024-02-12 18:55:08 +0800

    #13 - Public, Private & Readonly
    #13 - Public, Private & Readonly

--- public/app.js
+++ public/app.js
@@ -1,10 +1,13 @@
 "use strict";
 //classes
 class Invoice {
-    constructor(c, d, a) {
-        this.client = c;
-        this.details = d;
-        this.amount = a;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
+    constructor(client, details, amount) {
+        this.client = client;
+        this.details = details;
+        this.amount = amount;
     }
     format() {
         return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -15,10 +18,9 @@ const invTwo = new Invoice('mario', 'work on the mario website', 300);
 let invoices = [];
 invoices.push(invOne);
 invoices.push(invTwo);
-invOne.client = 'yoshi';
-invTwo.amount = 500;
-console.log(invOne, invTwo);
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 const form = document.querySelector('.new-item-form');
 //Inputs
 const type = document.querySelector('#type');
--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,14 @@
 //classes
 class Invoice {
-    client: string;
-    details: string;
-    amount: number;
+    // readonly client: string;
+    // private details: string;
+    // public amount: number;
 
-    constructor(c: string, d: string, a: number){
-        this.client = c;
-        this.details = d;
-        this.amount = a;
-    }
+    constructor(
+        readonly client: string,
+        private details: string,
+        public amount: number
+    ){}
 
     format(){
         return `${this.client} owes $${this.amount} for ${this.details}`;
@@ -22,12 +22,9 @@ let invoices: Invoice[] = [];
 invoices.push(invOne);
 invoices.push(invTwo);
 
-invOne.client = 'yoshi';
-invTwo.amount = 500;
-
-console.log(invOne, invTwo);
-
-console.log(invoices);
+invoices.forEach(inv => {
+    console.log(inv.client, inv.amount, inv.format());
+});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 
