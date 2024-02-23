7758b7d87aba3c804aa9eb764db3df44de894ebb
Ryan Clarian <rclarian@gmail.com>
2024-02-23 13:58:53 +0800

    #17 - Rendering an HTML Template
    #17 - Rendering an HTML Template

--- src/app.ts
+++ src/app.ts
@@ -1,38 +1,19 @@
 import { Invoice } from './classes/Invoice.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { Payment } from './classes/Payment.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('yoshi', 'plumbing work', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
-// console.log(docs);
-
-// const invOne = new Invoice('ryan', 'work on the ryan website', 250);
-// const invTwo = new Invoice('mario', 'work on the mario website', 300);
-
-// let invoices: Invoice[] = [];
-// invoices.push(invOne);
-// invoices.push(invTwo);
-
-// invoices.forEach(inv => {
-//     console.log(inv.client, inv.amount, inv.format());
-// });
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-
 
 //Inputs
 const type = document.querySelector('#type') as HTMLSelectElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+//list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
 
 form.addEventListener('submit', (e: Event) => {
     e.preventDefault();
@@ -45,5 +26,5 @@ form.addEventListener('submit', (e: Event) => {
         doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
     }
     
-    console.log(doc);
+    list.render(doc, type.value, 'end');
 })
